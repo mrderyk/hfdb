f36acbd005fc0b20474d695ef3553c33d6502610
Deryk DeGuzman <deryk.deguzman@gmail.com>
2021-06-29 07:36:00 -0700

    0.0.1 initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hfdb',
+    url='https://github.com/mrderyk/hfdb',
+    author='Deryk DeGuzman',
+    packages=find_packages(),
+    install_requires=['peewee'],
+    version='0.0.1',
+    license='UNLICENSED',
+    description='Hoops Fusion database modules',
+)
