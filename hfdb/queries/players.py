39db957098d04e82a31a773d86b4998a7dc899c2
Deryk DeGuzman <deryk.deguzman@gmail.com>
2021-07-01 20:36:57 -0700

    0.0.5 Add Player search query

--- hfdb/queries/players.py
+++ hfdb/queries/players.py
@@ -4,3 +4,10 @@ from .. models import Player
 
 def get_player_data(player_keys=[]):
   return Player.select().where(Player.key << player_keys)
+
+def get_players_by_search_string(str):
+  return Player.select() \
+    .where(
+      Player.searchable_tokens.contains(str)
+    )
+
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Deryk DeGuzman',
     packages=find_packages(),
     install_requires=['peewee'],
-    version='0.0.4',
+    version='0.0.5',
     license='UNLICENSED',
     description='Hoops Fusion database modules',
 )
